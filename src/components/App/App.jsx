a7b301ae4cbce1a6843b5a67a76393cbd9dfed27
Mykola-Veres <veresnickolay@gmail.com>
2023-04-26 22:12:42 +0300

    fix

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,5 +1,4 @@
 import { Navigate, Route, Routes } from 'react-router-dom';
-import { Toaster } from 'react-hot-toast';
 import Loader from '../Loader';
 import { Suspense } from 'react';
 import MainContainer from 'components/MainContainer/MainContainer';
@@ -11,7 +10,6 @@ export default function App() {
   return (
     <MainContainer>
       <GlobalStyle />
-      <Toaster position="top-center" reverseOrder={false} />
       <Suspense fallback={<Loader />}>
         <Routes>
           <Route path="/" element={<PizzaPage />} />
